8c6592421cb0c52bdb528ef2b9ca69cdfa798498
Nychtgian <nichtgian@gmail.com>
2019-06-20 16:05:13 +0200

    Worked on contents and add segmentet video

--- public/script/script.js
+++ public/script/script.js
@@ -5,9 +5,12 @@ const COLORPROPERTIES = [
 ];
 
 const SLIDEIMAGES = [
-  { src: "https://via.placeholder.com/200x100.png?text=Image 1", alt: "image 1 alt" },
-  { src: "https://via.placeholder.com/800x300.png?text=Image 2", alt: "image 2 alt" },
-  { src: "https://via.placeholder.com/300x500.png?text=Image 3", alt: "image 3 alt" }
+  { src: "landscape.jpg", alt: "landscape" },
+  { src: "view.jpg", alt: "view from our place" },
+  { src: "landscape3.jpg", alt: "landscape at sunset" },
+  { src: "street4.jpg", alt: "park with attractions" },
+  { src: "street8.jpg", alt: "street with threes" },
+  { src: "food-jbc.jpg", alt: "typical food" }
 ];
 
 const DAYINMS = 24 * 60 * 60 *1000;
@@ -31,7 +34,7 @@ function skipSlide(direction) {
 
   let newSlideImage = SLIDEIMAGES[currentSlideImage];
   let sliderElement = document.getElementById("sliderImage");
-  sliderElement.src = newSlideImage.src;
+  sliderElement.src = "src/image/" + newSlideImage.src;
   sliderElement.alt = newSlideImage.alt;
   sliderElement.style.animation = "none";
 
